e16817ea505be5db1cfb68a32e991a96782ce822
Tepminamop <fanbeyblade2001@gmail.com>
2022-09-23 22:53:57 +0300

    added pow

--- TOYAK/lab1/igors.cpp
+++ TOYAK/lab1/igors.cpp
@@ -2,6 +2,12 @@
 #include <string>
 #include <vector>
 #include <stack>
+using std::vector;
+using std::string;
+using std::stack;
+using std::cin;
+using std::cout;
+using std::endl;
 
 int get_symbol_priority(const char& symbol) {
 	switch (symbol) {
@@ -20,7 +26,7 @@ bool validate_expression() {
 	return true;
 }
 
-void erase_double_unary_minus(std::string& s) {
+void erase_double_unary_minus(string& s) {
 	int pos = 0;
 	while (pos >= 0 && pos < s.size()) {
 		pos = s.find("--");
@@ -29,27 +35,27 @@ void erase_double_unary_minus(std::string& s) {
 	}
 }
 
-void add_zero_for_unary_minus(std::string& s) {
+void add_zero_for_unary_minus(string& s) {
 	for (int i = 0; i < s.size(); i++) {
 		if (s[i] == '-' && i == 0) {
 			s.insert(0, "0");
 			i++;
 		}
-		else if (s[i] == '-') {
+		else if (s[i] == '-' && s[i - 1] == '(') {
 			s.insert(i, "0");
 			i++;
 		}
 	}
 }
 
-void parse_new_expression(std::string& s, std::string& exp) {
-	int pos = -1;
+void parse_new_expression(string& s, string& exp) {
+	int pos = 0;
 	while (pos >= 0 && pos < s.size()) {
 		int pos = s.find(exp);
-		std::string string_first_argument;
-		std::string string_second_argument;
-		double first_argument;
-		double second_argument;
+		if (pos < 0 || pos >= s.size()) break;
+
+		string string_first_argument;
+		string string_second_argument;
 
 		int tmp = pos + 4;
 		while (s[tmp] != ',') {
@@ -62,10 +68,24 @@ void parse_new_expression(std::string& s, std::string& exp) {
 			tmp++;
 		}
 
+		//now s[tmp] == ')'
+		string replace;
+		replace.append("((");
+		replace.append(string_first_argument);
+		replace.push_back(')');
+		replace.push_back('^');
+		replace.push_back('(');
+		replace.append(string_second_argument);
+		replace.append("))");
+
+		s.replace(pos, tmp - pos + 1, replace);
+
+		cout << "replace: " << replace << endl;
+		cout << "after pow: " << s << endl;
 	}
 }
 
-void push_element_to_stack(std::stack<char>& st, const char& symbol, std::string& polish) {
+void push_element_to_stack(stack<char>& st, const char& symbol, string& polish) {
 	if (st.empty()) {
 		st.push(symbol);
 		return;
@@ -99,9 +119,9 @@ void push_element_to_stack(std::stack<char>& st, const char& symbol, std::string
 	st.push(symbol);
 };
 
-std::string make_polish(std::string s) {
-	std::stack<char> st;
-	std::string polish = "";
+string make_polish(string s) {
+	stack<char> st;
+	string polish;
 
 	for (int i = 0; i < s.size(); i++) {
 		if (isdigit(s[i]) || s[i] == '.') {
@@ -140,12 +160,12 @@ std::string make_polish(std::string s) {
 	return polish;
 }
 
-std::vector<std::string> make_vector_from_string(std::string s) {
+vector<string> make_vector_from_string(string s) {
 	while (s[s.size() - 1] == ' ') {
 		s.erase(s.end() - 1);
 	}
 
-	std::vector<std::string> vec;
+	vector<string> vec;
 	while (s.find("  ") && s.size() != 1) {
 		int pos = s.find("  ");
 		if (pos < 0 || pos >= s.size()) break;
@@ -165,10 +185,10 @@ std::vector<std::string> make_vector_from_string(std::string s) {
 	return vec;
 }
 
-double calculate_polish(std::vector<std::string>& tokens) {
-	std::stack<double> st;
+double calculate_polish(vector<string>& tokens) {
+	stack<double> st;
 	for (auto& i : tokens) {
-		if (i == "+" || i == "-" || i == "*" || i == "/") {
+		if (i == "+" || i == "-" || i == "*" || i == "/" || i == "^") {
 			double e1 = st.top();
 			st.pop();
 
@@ -179,6 +199,7 @@ double calculate_polish(std::vector<std::string>& tokens) {
 			if (i == "-")  e1 = e2 - e1;
 			if (i == "*")  e1 = e2 * e1;
 			if (i == "/")  e1 = e2 / e1;
+			if (i == "^")  e1 = pow(e2, e1);
 
 			st.push(e1);
 		}
@@ -188,84 +209,31 @@ double calculate_polish(std::vector<std::string>& tokens) {
 	return st.top();
 }
 
-//double calculateRPN(std::vector<std::string>& tokens) {
-//	std::stack<double> s;
-//	for (auto& it : tokens) {
-//		if (priority(it[0]) > 1) {
-//			double e1 = s.top();
-//			s.pop();
-//
-//			if (!s.empty()) {
-//
-//				double e2 = s.top();
-//				s.pop();
-//
-//				switch (it[0]) {
-//				case '+':
-//					e1 = e1 + e2;
-//					break;
-//				case '-':
-//					/*if (::isdigit(e1)) {e1 = e2 - e1;}
-//					else
-//					{
-//						e1 = e2-e1;
-//					}*/
-//					e1 = e2 - e1;
-//
-//					break;
-//				case '*':
-//					e1 = e1 * e2;
-//					break;
-//				case '/':
-//					if (e2 == 0) {
-//						throw std::exception("X/0 - Error! Wrong argument");
-//						break;
-//					}
-//					e1 = e1 / e2;
-//					break;
-//				case '^':
-//					e1 = pow(e1, e2);
-//					break;
-//				}
-//				s.push(e1);
-//			}
-//
-//			else if (it == "-") {
-//				std::cout << e1 << std::endl;
-//				e1 = 0 - e1;
-//				s.push(e1);
-//			}
-//		}
-//		else {
-//			s.push(stod(it));
-//		}
-//	}
-//	return s.top();
-//}
-
-
 int main() {
-	std::string exp = "2+(1+(4))*(3+4)-5";//ans = 32
-	std::string exp3 = "-15*(-9*(-1)+11)-0";
-	exp3 = "-(-(-(5)))";
-	exp3 = "((-(--5.45)))";
-	exp3 = "-5";
-	//std::cin >> exp3;
+	string exp = "2+(1+(4))*(3+4)-5";//ans = 32
+	string exp3 = "-15*(-9*(-1)+11)-0";
+	//exp3 = "5-(-(-(5)))";
+	//exp3 = "((-(--5.45)))";
+	//exp3 = "5-5";
+	//cin >> exp3;
+	exp3 = "5*(-pow(2,3))+7";
+	string pow_expression = "pow";
+	parse_new_expression(exp3, pow_expression);
 
 	erase_double_unary_minus(exp3);
 	add_zero_for_unary_minus(exp3);
-	std::cout << exp3 << std::endl;
+	cout << exp3 << endl;
 
-	std::string ans;
+	string ans;
 	ans.append(make_polish(exp3));
 
-	std::cout << ans << std::endl;
+	cout << ans << endl;
 
-	std::vector<std::string> v = make_vector_from_string(ans);
+	vector<string> v = make_vector_from_string(ans);
 
 	double top = calculate_polish(v);
 
-	std::cout << top << std::endl;
+	cout << top << endl;
 
 	return 0;
 };
